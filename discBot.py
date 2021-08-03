c317d1bc156bdea23ff3c0ca5d2e6613175cd05b
Adam Nelson <n8592403@qut.edu.au>
2021-08-03 19:52:12 +1000

    Initial Commit

---
+++ discBot.py
@@ -0,0 +1,23 @@
+# bot.py
+import os
+import random
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+
+    if message.content.startswith("I'm") or message.content.startswith("im") or message.content.startswith("i'm") :
+        response = "Hi " + str(message.content[3:]).strip() + ", I'm dad!"
+        await message.channel.send(response)
+
+client.run(TOKEN)
