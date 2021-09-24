6f608f5c4fa56b00bc99c9db0f9d99f9c2be2418
Adam Nelson <n8592403@qut.edu.au>
2021-09-24 13:44:20 +1000

    fix to .jpg regex not completely correct

--- caretaker.py
+++ caretaker.py
@@ -31,7 +31,7 @@ async def on_message(message):
     if message.author == bot.user:
         return
 
-    check = re.compile(r"(http.*.jpg)")
+    check = re.compile(r"(http.*[.]jpg)")
     match = check.search(message.content)
     if match:
         msg = message.content
